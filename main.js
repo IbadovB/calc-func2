2e4ed310c64788637fc7e6c09224b00dbfba1634
IbadovB <ibadovb@gmail.com>
2024-06-26 09:58:20 +0400

    second

--- main.js
+++ main.js
@@ -1,30 +1,33 @@
+let numb1 = parseInt(document.querySelector("#numb1").value);
+let numb2 = parseInt(document.querySelector("#numb2").value);
+let operators = document.querySelector("#operators").value;
+let fail = document.querySelector(".wrong").innerHTML;
+
 
 function calc() {
-    let numb1 = parseInt(document.getElementById('numb1').value);
-    let numb2 = parseInt(document.getElementById('numb2').value);
-    let operators = document.getElementById('operators').value;
-
-    if (operators === '+') {
-        document.getElementById('result').value = numb1 + numb2;
+      if (operators === '+') {
+        document.querySelector("#result").value = numb1 + numb2;
     }
-
     if (operators === '-') {
-        document.getElementById('result').value = numb1 - numb2;
+        document.querySelector("#result").value = numb1 - numb2;
     }
-
     if (operators === '/') {
-        document.getElementById('result').value = numb1/numb2;
+        document.querySelector("#result").value = numb1 / numb2;   
     }
-
     if (operators === 'X') {
-        document.getElementById('result').value = numb1*numb2;
+        document.querySelector("#result").value = numb1 * numb2;
     }
-    }
-
-
-
-function numberonly (input) {
-    let num = /[^0-9]/gi;
-    input.value = input.value.replace (num, "");
-  
 }
+
+function numberOnly(input) {
+    let num = /[^0-9]/gi;
+    input.value = input.value.replace(num, "");
+}
+
+function chechk(input) {
+    let wrong='failed';
+if (input = '') {
+    return wrong
+}
+}
+
